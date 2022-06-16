ef1ec2d72258d18d11ec0ba5153e91fd4b228a9e
Vojtech Sedlak <vojtech@linden.local>
2022-06-16 15:41:25 -0700

    fixing img

--- scroll.js
+++ scroll.js
@@ -176,9 +176,18 @@
             console.log(data);
             var url = data[0].og_url.split('?')[0];
             var title = data[0].og_title;
-            var cta = 'Take Action';
+            switch(data[0].en_type) {
+              case 'dc':
+                var cta = 'Sign the petition';
+                break;
+              case 'et':
+                var cta = 'Send a message';
+                break;
+              default:
+                var cta = 'Take action';
+            }
             var img = data[0].og_img;
-            var el = '<div class="dc_btn gtm_event" data-event="petition-scroll-dc_'+i+'" id="dc'+i+' style="background-image:url('+img+');background-position:center;background-size:cover;position:relative;"><div class="overlay"><a href="'+url+'?chain&amp;ea.tracking.id=act-page&amp;xvar=dc" target="_new"><div class="dc_content"><h2>'+title+'</h2></div><div class="dc_cta"><h3>'+cta+'</h3></div></a></div></div>';
+            var el = '<div class="dc_btn gtm_event" data-event="petition-scroll-dc_'+i+'" id="dc'+i+' style="background-image:url("'+img+'");background-position:center;background-size:cover;position:relative;"><div class="overlay"><a href="'+url+'?chain&amp;ea.tracking.id=act-page&amp;xvar=dc" target="_new"><div class="dc_content"><h2>'+title+'</h2></div><div class="dc_cta"><h3>'+cta+'</h3></div></a></div></div>';
             $('.dc_btns').append(el);
         });
       })
