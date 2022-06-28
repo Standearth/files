d6dd4d2d408e5b52cb27c2c6edd12eef970402c4
Vojtech Sedlak <vojtech@linden.local>
2022-06-28 09:51:27 -0700

    adding geo filtering

--- scroll.js
+++ scroll.js
@@ -206,7 +206,7 @@
             url: "https://stand-en-api.herokuapp.com/api/pages?geo="+o_geo+"&status=live",
           }).done(function(data) {
             var filtered = data.filter(function(d) {
-              if (d.st_campaign.toLowerCase() != o_campaign.toLowerCase() & d.og_url == 'ndull' & d.og_title !== null & d.og_img !== null) {
+              if (d.st_campaign.toLowerCase() != o_campaign.toLowerCase() & d.og_url !== null & d.og_title !== null & d.og_img !== null) {
                 return true;
               } else {
                 return false;
