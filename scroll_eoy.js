268aceae9fb577e094778699d57a5bff4f4d479b
Vojtech Sedlak <vojtech@linden.local>
2023-11-24 08:47:06 -0800

    adding chain to links

--- scroll_eoy.js
+++ scroll_eoy.js
@@ -292,17 +292,17 @@
   function donate_redirect(monthly) {
     if (monthly) {
       if (country == 'CA') {
-        location.href = 'https://act.stand.earth/page/60232/donate/1?xvar=scroll';
+        location.href = 'https://act.stand.earth/page/60232/donate/1?chain&xvar=scroll';
       } else {
         console.log('us');
-        location.href = 'https://act.stand.earth/page/60233/donate/1?xvar=scroll';
+        location.href = 'https://act.stand.earth/page/60233/donate/1?chain&xvar=scroll';
       }
     } else {
       console.log('onetime');
       if (country == 'CA') {
-        location.href = 'https://act.stand.earth/page/59944/donate/1?xvar=scroll';
+        location.href = 'https://act.stand.earth/page/59944/donate/1?chain&xvar=scroll';
       } else {
-        location.href = 'https://act.stand.earth/page/59970/donate/1?xvar=scroll';   
+        location.href = 'https://act.stand.earth/page/59970/donate/1?chain&xvar=scroll';   
       }
     }
  }
